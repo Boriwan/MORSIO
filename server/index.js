ce8a60bd0718bfde3c257b015dc241545010005e
unknown <oliver.cavallo@educanet.cz>
2024-03-05 19:21:52 +0100

    mongo connected

--- server/index.js
+++ server/index.js
@@ -1,56 +1,22 @@
+require('dotenv').config();
 
-const PostController = require("./controllers/post");
-const CategoryController = require("./controllers/category");
-const CommentController = require("./controllers/comment");
-const LikeController = require("./controllers/like");
-const UserController = require("./controllers/user");
-var express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require('express');
+const mongoose = require('mongoose');
 
-var app = express();
-const mongoose = require("mongoose");
+const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const mongoDBUri =
-    "mongodb+srv://Oliver:admin1234@cluster0.uaaymiq.mongodb.net/";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(mongoDBUri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Error connecting to MongoDB', err);
 });
-app.use(cors());
 
-mongoose
-    .connect(mongoDBUri)
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => console.error("MongoDB connection error:", err));
-
-// app.use((req, res, next) => {
-//   req.user = {
-//     uuIdentity: "28-4201-1",
-//     uuIdentityName: "Jakub Stádník",
-//     role: "admin",
-//   };
-//   next();
-// });
-// Add error handling for multer
-
-//default port
-const port = 8000;
-
-app.use("/post", PostController);
-app.use("/category", CategoryController);
-app.use("/comment", CommentController);
-app.use("/likes", LikeController);
-app.use("/user", UserController);
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
