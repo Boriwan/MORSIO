17073f9062a2e6fdb554bbc2c2799feb23119b3c
EduRobin <robin.cavallo@educanet.cz>
2024-04-11 15:53:42 +0200

    sessionlist

--- client/src/Components/SessionList/SessionList.js
+++ client/src/Components/SessionList/SessionList.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import '../SessionList/SessionList.css'
+import img from '../../images/Logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleUser, faCog, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
-const SessionList = () => {
+
+function SessionList() {
   return (
-    <div>
-      <h2>Sessions List</h2>
-      {/* Zde bude seznam session */}
-      {/* Zde budou tlačítka pro přidání nové session */}
+    <div className="session-list-container">
+      <div className="session-list-header">
+        <img src={img} alt="Morsio Logo" className="session-list-logo" />
+        <h1>Morsio</h1>
+      </div>
+      <div className='session-list-body'>
+        <h2 className='session-list-title'>Session List</h2>
+        <button className="session-add-btn">Add new session</button>
+        <div className="session-list-sessions">
+          <p>A</p>
+          <p>A</p>
+          <p>A</p>
+          <p>A</p>
+          <p>A</p>
+        </div>
+      </div>
+      <div className="session-list-footer">
+        <FontAwesomeIcon icon={faCircleUser} className="icon profile-icon" />
+
+        <FontAwesomeIcon icon={faInfoCircle} className="icon info-icon" />
+
+        <FontAwesomeIcon icon={faCog} className="icon settings-icon" />
+
+       
+      </div>
     </div>
   );
 }
