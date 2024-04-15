02666b83832cfadb66039571477f752f08460465
stefan-rajilic <stefan.rajilic@seznam.cz>
2024-04-15 20:09:30 +0200

    login, register, home, app.js = just routes

--- client/src/src/Components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "./Button.css"
-
-export default function Button() {
-  return (
-    <div>Button</div>
-  )
-}
--- client/src/src/Components/SessionTile/SessionTile.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import "./SessionTile.css"
-
-export default function SessionTile() {
-  return (
-    <div>SessionTile</div>
-  )
-}
