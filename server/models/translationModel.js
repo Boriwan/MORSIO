5551cd48d59f5e98e9825b9f8e983f2047064afd
Jakub Stadnik <106155603+jakubstadnik03@users.noreply.github.com>
2024-04-15 19:51:50 +0200

    fix date

--- server/models/translationModel.js
+++ server/models/translationModel.js
@@ -6,6 +6,7 @@ const translationSchema = new mongoose.Schema({
   sessionId: { type: String, required: true }, // ID of the session
   morseCode: { type: Array, required: true }, 
   translation: { type: String, required: true},
+  creationDate: { type: Date, required: true}
 });
 
 const Translation = mongoose.model('Translation', translationSchema);
