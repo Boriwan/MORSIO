f343a260a3dae227e42cf257f67e37b0b39104b4
Boriwan <boris.bocek@educanet.com>
2024-04-18 16:56:01 +0200

    minor updates and tweaks

--- client/src/Pages/Session/Session.js
+++ client/src/Pages/Session/Session.js
@@ -1,14 +1,26 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Session.css";
 import ChatComponent from "../../Components/ChatComponent/ChatComponent";
+import MorsioSheet from "../../Components/MorsioSheet/MorsioSheet";
 
 function Session(props) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="session-page">
-      <div className="session-page-container">
-        <ChatComponent/>
+    <>
+      <div className="session-page">
+        <div className="session-page-container">
+          <ChatComponent />
+        </div>
       </div>
-    </div>
+      <div className="morsio-cheatsheet">
+        <MorsioSheet isOpen={isOpen} toggleSidebar={toggleSidebar} />
+      </div>
+    </>
   );
 }
 
