edfa05aabe2c0d71c1162076298f00aff7c863d0
Jakub Stadnik <106155603+jakubstadnik03@users.noreply.github.com>
2024-06-10 16:24:29 +0200

    connect to api

--- client/src/apiService.js
+++ client/src/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://morsio.lm.r.appspot.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
