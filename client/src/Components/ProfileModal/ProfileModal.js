211677dd79c74ffa389c2b7035442d89c4d08aee
Boriwan <boris.bocek@educanet.com>
2024-06-11 19:46:29 +0200

    back

--- client/src/Components/ProfileModal/ProfileModal.js
+++ client/src/Components/ProfileModal/ProfileModal.js
@@ -1,31 +1,24 @@
 // ProfileModal.js
 import React from "react";
-import "./ProfileModal.css";
+import './ProfileModal.css'; // Přidáno vlastní CSS
 
 const ProfileModal = ({ isOpen, onClose, username, email }) => {
   if (!isOpen) return null;
 
   return (
-    <>
-        <div className="modal">
-          <button onClick={onClose} className="profile-modal-close-btn">
-            X
-          </button>
-          <div className="user-info">
-            <p>
-              <strong>Username:</strong> {username}
-            </p>
-            <p>
-              <strong>Email:</strong> {email}
-            </p>
-          </div>
-          <div className="modal-actions">
-            <button className="modal-action-button" onClick={() => {}}>
-              Log Out
-            </button>
-          </div>
+    <div className="modal-overlay">
+      <div className="modal">
+        <button onClick={onClose} className="profile-modal-close-btn">X</button>
+        <h2>User Profile</h2>
+        <div className="user-info">
+          <p><strong>Username:</strong> {username}</p>
+          <p><strong>Email:</strong> {email}</p>
         </div>
-    </>
+        <div className="modal-actions">
+          <button className="modal-action-button" onClick={() => {}}>Log Out</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
