e928661ab8f100a31ed7ee3122bcae2023544b4d
Boriwan <boris.bocek@educanet.com>
2024-06-12 14:14:19 +0200

    battery progress

--- client/src/Components/ProfileModal/ProfileModal.js
+++ client/src/Components/ProfileModal/ProfileModal.js
@@ -1,20 +1,78 @@
-import React from "react";
-import './ProfileModal.css'; 
+import React, { useState, useEffect } from "react";
+import "./ProfileModal.css";
 
 const ProfileModal = ({ isOpen, onClose, username, email }) => {
-  if (!isOpen) return null;
+  const [battery, setBattery] = useState(null);
+  const [batteryStatus, setBatteryStatus] = useState(null);
+
+  useEffect(() => {
+    const batterySocket = new WebSocket("ws://localhost:1880/ws/battery");
+    const batteryStatusSocket = new WebSocket(
+      "ws://localhost:1880/ws/batteryStatus"
+    );
+
+    batterySocket.onopen = () => {
+      console.log("Battery WebSocket connection opened");
+    };
+
+    batterySocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setBattery(data.battery);
+    };
+
+    batterySocket.onclose = () => {
+      console.log("Battery WebSocket connection closed");
+    };
+
+    batterySocket.onerror = (error) => {
+      console.error("Battery WebSocket error:", error);
+    };
+
+    batteryStatusSocket.onopen = () => {
+      console.log("Battery Status WebSocket connection opened");
+    };
+
+    batteryStatusSocket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      setBatteryStatus(data.batteryStatus);
+    };
+
+    batteryStatusSocket.onclose = () => {
+      console.log("Battery Status WebSocket connection closed");
+    };
+
+    batteryStatusSocket.onerror = (error) => {
+      console.error("Battery Status WebSocket error:", error);
+    };
+
+    return () => {
+      batterySocket.close();
+      batteryStatusSocket.close();
+    };
+  }, []);
 
   return (
     <div className="modal-overlay">
       <div className="modal">
-        <button onClick={onClose} className="profile-modal-close-btn">X</button>
         <h2>User Profile</h2>
         <div className="user-info">
-          <p><strong>Username:</strong> {username || "Unavailable"}</p>
-          <p><strong>Email:</strong> {email || "Unavailable"}</p>
+          <p>
+            <strong>Username:</strong> {username || "Unavailable"}
+          </p>
+          <p>
+            <strong>Email:</strong> {email || "Unavailable"}
+          </p>
+          <p>
+            <strong>Battery at </strong> {battery || "Unavailable"} %
+          </p>
+          <p>
+            <strong>Status: </strong> {batteryStatus || "Unavailable"}
+          </p>
         </div>
         <div className="modal-actions">
-          <button className="modal-action-button" onClick={onClose}>Close</button>
+          <button className="modal-action-button" onClick={onClose}>
+            Close
+          </button>
         </div>
       </div>
     </div>
